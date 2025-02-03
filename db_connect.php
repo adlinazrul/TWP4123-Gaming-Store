1f7a68f39bae3f69b3f821d7a74e44a893d40c07
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-02-03 16:02:40 +0800

    try

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "users";
+$servername = "localhost";  // Database server
+$username = "root";         // Database username (usually 'root' in local development)
+$password = "";             // Database password (empty by default in XAMPP)
+$dbname = "gaming_store";   // Your actual database name
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,5 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
