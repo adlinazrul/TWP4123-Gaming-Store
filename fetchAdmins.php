23c075c5b5c437bc0605abee8adb1bf61936619c
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-02-07 22:00:06 +0800

    try

--- fetchAdmins.php
+++ fetchAdmins.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM admin_users";
+$sql = "SELECT * FROM admin_list";
 $result = $conn->query($sql);
 
 $admins = [];
