d94629ae8c5ada58bb4f092330141ed7c1d72398
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-05-01 22:55:48 +0800

    try

--- addadmin.php
+++ addadmin.php
@@ -69,8 +69,8 @@ $result = $conn->query($sql);
 		<li><a href="admindashboard.html"><i class='bx bxs-dashboard'></i><span class="text">Dashboard</span></a></li>
 		<li><a href="manageproduct.php"><i class='bx bxs-shopping-bag-alt'></i><span class="text">Product Management</span></a></li>
 		<li><a href="order.html"><i class='bx bxs-doughnut-chart'></i><span class="text">Order</span></a></li>
-		<li class="active"><a href="#"><i class='bx bxs-message-dots'></i><span class="text">Message</span></a></li>
-		<li><a href="admindashboard.php"><i class='bx bxs-group'></i><span class="text">Admin</span></a></li>
+		<li><a href= "#"><i class='bx bxs-message-dots'></i><span class="text">Message</span></a></li>
+		<li class="active"><a href= "#"><i class='bx bxs-group'></i><span class="text">Admin</span></a></li>
 	</ul>
 	<ul class="side-menu">
 		<li><a href="#"><i class='bx bxs-cog'></i><span class="text">Settings</span></a></li>
