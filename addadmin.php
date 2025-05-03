137674e315ce9ea0c84a39a08a6e6bcb320ee2f2
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-05-03 13:36:02 +0800

    try

--- addadmin.php
+++ addadmin.php
@@ -56,7 +56,7 @@ $result = $conn->query($sql);
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-	<link rel="stylesheet" href="managestaff.css">
+	<link rel="stylesheet" href="admindashboard.css">
     
 
 	<title>Admin</title>
