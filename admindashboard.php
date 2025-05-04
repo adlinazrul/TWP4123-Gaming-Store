67b90501ea4d6b10c581ce680eebbb16fb85a759
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-05-04 23:40:11 +0800

    try

--- admindashboard.php
+++ admindashboard.php
@@ -4,7 +4,7 @@ session_start();
 // Check if user is logged in
 if (!isset($_SESSION['admin_id'])) {
     // Redirect to login page if not logged in
-    header("Location: login_admin.php");
+    header("Location: loginadmin.html");
     exit();
 }
 
