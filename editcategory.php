15c6efa4fe189497f68bcaafda19cd0d22563e6d
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-05-12 20:54:26 +0800

    try

--- editcategory.php
+++ editcategory.php
@@ -4,10 +4,7 @@ session_start();
 include 'db_connection.php';
 
 // Check if user is logged in
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+
 
 $success_message = $error_message = "";
 $category_name = "";
