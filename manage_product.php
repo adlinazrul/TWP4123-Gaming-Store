4bfd224e2eb28d6089f6a29841b6830975592532
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-05-26 22:54:48 +0800

    try

--- manage_product.php
+++ manage_product.php
@@ -133,11 +133,31 @@ $result = mysqli_query($conn, $sql);
 	</a>
 	<ul class="side-menu top">
 		<li><a href="dashboard.php"><i class='bx bxs-dashboard'></i><span class="text">Dashboard</span></a></li>
-		<li class="active"><a href="manage_product.php"><i class='bx bxs-shopping-bag-alt'></i><span class="text">Product Management</span></a></li>
-		<li><a href="managecategory.php"><i class='bx bxs-category'></i><span class="text">Category Management</span></a></li>
-		<li><a href="order_admin.php"><i class='bx bxs-doughnut-chart'></i><span class="text">Order</span></a></li>
-		<li><a href="cust_list.php"><i class='bx bxs-user'></i><span class="text">Customer</span></a></li>
-		<li><a href="view_admin.php"><i class='bx bxs-group'></i><span class="text">Admin</span></a></li>
+		<li>
+                <a href="cust_list.php">
+                    <i class='bx bxs-user'></i>
+                    <span class="text">Customer</span>
+                </a>
+            </li>
+            <li>
+                <a href="managecategory.php">
+                    <i class='bx bxs-category'></i>
+                    <span class="text">Category Management</span>
+                </a>
+            </li>
+            <li class="active">
+                <a href="manage_product.php">
+                    <i class='bx bxs-shopping-bag-alt'></i>
+                    <span class="text">Product Management</span>
+                </a>
+            </li>
+            
+            <li>
+                <a href="order_admin.php">
+                    <i class='bx bxs-doughnut-chart'></i>
+                    <span class="text">Order</span>
+                </a>
+            </li>
 	</ul>
 	<ul class="side-menu">
 		<li><a href="#"><i class='bx bxs-cog'></i><span class="text">Settings</span></a></li>
