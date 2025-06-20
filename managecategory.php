0bfb07e010c8fd9ec35be8f43e187c8ac6807443
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-06-20 23:06:14 +0800

    trt

--- managecategory.php
+++ managecategory.php
@@ -201,12 +201,6 @@ $result = $conn->query($sql);
 	</ul>
 	<ul class="side-menu">
 		<li>
-			<a href="#">
-				<i class='bx bxs-cog'></i>
-				<span class="text">Settings</span>
-			</a>
-		</li>
-		<li>
 			<a href="?logout=1" class="logout">
 				<i class='bx bxs-log-out-circle'></i>
 				<span class="text">Logout</span>
