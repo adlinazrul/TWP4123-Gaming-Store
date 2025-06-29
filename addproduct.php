9fb007064b4213be9e99f56b227b89df58e8aac4
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-06-29 22:16:39 +0800

    try

--- addproduct.php
+++ addproduct.php
@@ -87,7 +87,7 @@ $result = $conn->query("SELECT * FROM products");
 
          <input type="file" accept="image/png, image/jpeg, image/jpg" name="product_image" class="box" required />
          <input type="submit" class="btn" name="add_product" value="Add Product" />
-         <a href="manageproduct.php" class="back-link">← Back to Product List</a>
+         <center><a href="manageproduct.php" class="back-link">← Back to Product List</a></center>
       </form>
    </div>
 
