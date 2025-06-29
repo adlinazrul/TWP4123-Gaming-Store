09685f25d76b5d6574acf0a265d4297704a0901f
Ali AhKao or Muthu <1231200872@mmu.edu.my>
2025-06-29 21:34:35 +0800

    try

--- login_admin.php
+++ login_admin.php
@@ -588,7 +588,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </form>
         
         <div class="footer">
-            <button type="button" class="cancel-btn"><a href="index.php.php"><i class="fas fa-times"></i> Cancel</button>
+            <button type="button" class="cancel-btn"><a href="index.html"><i class="fas fa-times"></i> Cancel</button>
             <p>© 2023 Gaming Store. All rights reserved.</p>
         </div>
     </div>
